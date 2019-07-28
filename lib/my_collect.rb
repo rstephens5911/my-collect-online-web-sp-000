560a87eaefae6740bcb265144d26b27563d267d5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 17:23:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(array)
+def my_collect(collection)
   i = 0
 
-  while i < array.length
-    yield array[i]
+  while i < collection.length
+    yield collection[i]
     i = i + 1
   end
-  array
+  collection
 end
